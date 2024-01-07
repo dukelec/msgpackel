b76dfb0f636c32d8590417725d321fd917d8fe3c
Duke <d@d-l.io>
2024-01-07 13:38:33 +0800

    tools: using pprint

--- tools/msgpack_big2little.py
+++ tools/msgpack_big2little.py
@@ -8,10 +8,11 @@
 
 import os, sys
 import umsgpack
+import pprint
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from msgpackel_py import umsgpackel
 
-if len(sys.argv) != 3:
+if len(sys.argv) < 2:
     print("Usage: ./msgpack_big2little.py input.mpk output.mpkl")
     exit(-1)
 
@@ -19,8 +20,10 @@ with open(sys.argv[1], 'rb') as f:
     in_file = f.read()
 
 in_data = umsgpack.unpackb(in_file)
+pprint.pp(in_data)
 out_data = umsgpackel.packb(in_data)
 
-with open(sys.argv[2], 'wb') as f:
-    f.write(out_data)
+if len(sys.argv) >= 3:
+    with open(sys.argv[2], 'wb') as f:
+        f.write(out_data)
 
